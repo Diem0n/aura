595dab9f672efd0a6fc132a7d25a5808383b4b63
shy-tan <iabdullahbintahir@gmail.com>
2022-03-05 23:51:30 +0500

    intiate home-page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ function App() {
       "url(https://source.unsplash.com/1600x900/?nature)";
   }, []);
 
-  return <></>;
+  return <>
+  <div className="container" style={{color:'white', fontWeight:'bolder', fontSize:'70px'}}>
+    Good Morning, Levi
+  </div>
+  </>;
 }
 
 export default App;
