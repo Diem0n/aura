49fb5bc842a07f5f1d2ac06f0d4d8fd096d13a01
shy-tan <iabdullahbintahir@gmail.com>
2022-03-07 22:19:45 +0500

    improve ui

--- src/App.js
+++ src/App.js
@@ -8,17 +8,44 @@ function App() {
         setDate(new Date());
       },
       1000
-    )
-  },[]);
+    );
+  //   const onStorage = () => {
+  //     setName(localStorage.getItem('username')||"N/A");
+  // };
+    // window.addEventListener('storage', onStorage);
+  }, []);
 
   const [date, setDate] = useState(new Date());
+  // todo show name input only if localstorage does not have the username
+  const [name, setName] = useState(localStorage.getItem('username') || "N/A");
+  const [nameText, setNameText] = useState("");
+  const [isNameSubmitted, setIsNameSubmitted] = useState(localStorage.getItem('username') ? true:false);
+  
+  const userNameSubmitHandler = e => {
+    e.preventDefault();
+    // todo 
+    // ! juggar for now
+    localStorage.setItem('username', nameText);
+    setName(localStorage.getItem('username') || "N/A");
+    setIsNameSubmitted(true);
+
+  }
+
+  const nameTextChangeHandler = e => {
+    e.preventDefault();
+    setNameText(e.target.value);
+  }
 
   return (
     <>
-    
-      <div style={{ color: "white", fontWeight: "bolder", fontSize: "70px" }}>
-        Good Morning, Levi
-        <div style={{ color: "white" }}>
+      <div className="flex-col justify-center align-middle items-center ">
+
+        {/* {isNameSubmitted && <div style={{ fontWeight: "bolder", fontSize: "70px" }}> */}
+        {<div className="text-right text-3xl">Weather placeholder</div>}
+        {<div className="text-center text-9xl">
+          Good Morning, {name}
+        </div>}
+        <div className="text-white  text-center font-extrabold text-9xl p-15">
           {date.toLocaleString("en-GB", {
             hour: "numeric",
             minute: "numeric",
@@ -26,9 +53,12 @@ function App() {
             hour12: true,
           })}
         </div>
-        <input type="text" id="fname" name="fname"/>
-
-      </div>
+        <div className="text-center text-6xl justify-end">
+          <form onSubmit={userNameSubmitHandler}>
+          {/* {!isNameSubmitted &&  <input type="text" placeholder="What's your name?" className="border-b-8 border-b-white outline-none bg-transparent" onChange={nameTextChangeHandler} />} */}
+          {<input type="text" placeholder="What's your name?" className="border-b-8 border-b-white outline-none bg-transparent" onChange={nameTextChangeHandler} />}
+          
+          </form></div></div>
     </>
   );
 }
